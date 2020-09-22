0e595aad128e9f3156ad07a936f7c906b0133e6f
刘则昌 <914316848@qq.com>
2020-09-22 20:55:42 +0800

    ...

--- vertx-plus-common/src/main/java/plus/vertx/core/support/EventBusUtil.java
+++ vertx-plus-common/src/main/java/plus/vertx/core/support/EventBusUtil.java
@@ -1,11 +1,12 @@
 package plus.vertx.core.support;
 
 import com.google.common.base.Strings;
-import com.google.protobuf.Message;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
 import java.util.Objects;
 import org.slf4j.Logger;
@@ -18,47 +19,47 @@ import org.slf4j.LoggerFactory;
 public class EventBusUtil {
      final private static Logger LOGGER = LoggerFactory.getLogger(EventBusUtil.class);
     private static EventBus singletonEventBus;
-    private static DeliveryOptions deliveryOptions;
 
-    public EventBusUtil() {
+    public EventBusUtil(Vertx vertx) {
+        singletonEventBus = vertx.eventBus();
     }
 
-    public static void init(EventBus eventBus) {
-        //默认40秒
-        init(eventBus,8000);
-    }
-
-    public static void init(EventBus eventBus,long sendTimeoutMs) {
-        Objects.requireNonNull(eventBus, "未初始化EventBus");
-        singletonEventBus = eventBus;
-        deliveryOptions = new DeliveryOptions();
-        deliveryOptions.setSendTimeout(sendTimeoutMs);
-    }
-
-    public static EventBus getEventBusInstance() {
+    public static EventBus getInstance() {
         Objects.requireNonNull(singletonEventBus, "未初始化EventBus");
         return singletonEventBus;
     }
 
     /**
      * 发送消息，并收取回调
-     * @param address
-     * @param params
+     * @param <T>
+     * @param address 地址
+     * @param params 参数
+     * @param sendTimeoutMs 超时时间，单位毫秒
      * @return replyHandler
      */
-    public static Future<Message> send(String address, JsonObject params){
-        Promise<Message> result = Promise.promise();
-        if(Strings.isNullOrEmpty(address)||null==params||params.isEmpty()){
-            LOGGER.error("缺少请求参数，调用消息失败");
-            result.fail("缺少请求参数，调用消息失败");
-        }else{
-            getEventBusInstance().request(address, params, deliveryOptions, replyHandler->{
+    public static <T> Future<Message<T>> send(String address, JsonObject params,long sendTimeoutMs){
+        Promise<Message<T>> result = Promise.promise();
+        String errorMsg = "";
+        if(ValidateUtil.isEmpty(address)){
+            errorMsg = "缺少请求地址，调用消息失败";
+        } else if (null==params||params.isEmpty()) {
+            errorMsg = "缺少请求参数，调用消息失败";
+        } else {
+            DeliveryOptions deliveryOptions = new DeliveryOptions();
+            deliveryOptions.setSendTimeout(sendTimeoutMs);
+            getInstance().<T>request(address, params, deliveryOptions, replyHandler->{
                 if (replyHandler.succeeded()) {
-                    result.complete((Message) replyHandler.result());
+                    result.complete((Message<T>) replyHandler.result());
                 } else {
+                    LOGGER.error("\n 消息地址:{} \n 参数:{} \n ",address,params.encodePrettily());
+                    LOGGER.error("",replyHandler.cause());
                     result.fail(replyHandler.cause());
                 }
             });
+        }
+        if (ValidateUtil.isNotEmpty(errorMsg)) {
+            LOGGER.error(errorMsg);
+            result.fail(errorMsg);
         }
         return result.future();
     }
---
+++ vertx-plus-common/src/main/java/plus/vertx/core/support/VertxUtil.java
@@ -0,0 +1,198 @@
+package plus.vertx.core.support;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Verticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.eventbus.EventBusOptions;
+import io.vertx.core.json.JsonObject;
+import java.io.File;
+import java.io.IOException;
+import java.util.function.Consumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Vertx启动工具类
+ * @author crazyliu
+ */
+public class VertxUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(VertxUtil.class);
+    
+    /**
+     * 获取vertx
+     * @return
+     */
+    public static Vertx getVertx() {
+        EventBusOptions eventBusOptions = new EventBusOptions();
+        eventBusOptions.setConnectTimeout(400000);
+        VertxOptions vertxOptions = new VertxOptions();
+        vertxOptions
+                .setEventBusOptions(eventBusOptions)
+                .setBlockedThreadCheckInterval(999999999L)
+                .setEventLoopPoolSize(20)
+                .setMaxEventLoopExecuteTime(Long.MAX_VALUE)
+                .setWorkerPoolSize(256)
+                .setMaxWorkerExecuteTime(Long.MAX_VALUE)
+                .setWarningExceptionTime(Long.MAX_VALUE)
+                .setPreferNativeTransport(true);
+        Vertx vertx = Vertx.vertx(vertxOptions);
+        return vertx;
+    }
+    
+    /**
+     * 执行verticle类
+     * @param verticle
+     * @param vertx
+     * @param deploymentOptions
+     * @return 
+     */
+    public static Future<String> runFull(Verticle verticle, Vertx vertx, DeploymentOptions deploymentOptions) {
+        Promise<String> result = Promise.promise();
+        Consumer<Vertx> runner = (Vertx vert) -> {
+            try {
+                if (deploymentOptions != null) {
+                    vert.deployVerticle(verticle, deploymentOptions, result);
+                } else {
+                    vert.deployVerticle(verticle, result);
+                }
+            } catch (Throwable t) {
+                LOGGER.error("",t);
+            }
+        };
+        runner.accept(vertx);
+        return result.future();
+    }
+    
+    /**
+     * 执行verticle类
+     * @param clazz
+     * @param vertx
+     * @param deploymentOptions
+     * @return 
+     */
+    public static Future<String> runFull(Class clazz, Vertx vertx, DeploymentOptions deploymentOptions) {
+        String exampleDir = "/src/main/java/" + clazz.getPackage().getName().replace(".", "/");
+        String verticleID = clazz.getName();
+        try {
+            // We need to use the canonical file. Without the file name is .
+            File current = new File(".").getCanonicalFile();
+            if (exampleDir.startsWith(current.getName()) && !exampleDir.equals(current.getName())) {
+                exampleDir = exampleDir.substring(current.getName().length() + 1);
+            }
+        } catch (IOException e) {
+            // Ignore it.
+            LOGGER.error("",e);
+        }
+
+        Promise<String> result = Promise.promise();
+        System.setProperty("vertx.cwd", exampleDir);
+        Consumer<Vertx> runner = vert -> {
+            try {
+                if (deploymentOptions != null) {
+                    vert.deployVerticle(verticleID, deploymentOptions, result);
+                } else {
+                    vert.deployVerticle(verticleID, result);
+                }
+            } catch (Throwable t) {
+                LOGGER.error("",t);
+            }
+        };
+        runner.accept(vertx);
+        return result.future();
+    }
+    
+    /**
+     * 执行verticle类
+     * @param verticle
+     * @return 
+     */
+    public static Future<String> run(Verticle verticle) {
+        return run(verticle, new JsonObject());
+    }
+
+    /**
+     * 执行verticle类
+     * @param verticle
+     * @param config
+     * @return 
+     */
+    public static Future<String> run(Verticle verticle, JsonObject config) {
+        return run(verticle, getVertx(), config);
+    }
+
+    /**
+     * 执行verticle类
+     * @param verticle
+     * @param vertx
+     * @return 
+     */
+    public static Future<String> run(Verticle verticle, Vertx vertx) {
+        return run(verticle, vertx, new JsonObject());
+    }
+
+    /**
+     * 执行verticle类
+     * @param verticle
+     * @param vertx
+     * @param config
+     * @return 
+     */
+    public static Future<String> run(Verticle verticle, Vertx vertx, JsonObject config) {
+        if (config.isEmpty()) {
+            return runFull(verticle, vertx,null);
+        } else {
+            DeploymentOptions deploymentOptions = new DeploymentOptions();
+            deploymentOptions.setConfig(config);
+            return runFull(verticle, vertx, deploymentOptions);
+        }
+    }
+    
+    /**
+     * 执行verticle类
+     * @param clazz
+     * @return 
+     */
+    public static Future<String> run(Class clazz) {
+        return run(clazz, new JsonObject());
+    }
+
+    /**
+     * 执行verticle类
+     * @param clazz
+     * @param config
+     * @return 
+     */
+    public static Future<String> run(Class clazz, JsonObject config) {
+        return run(clazz, getVertx(), config);
+    }
+
+    /**
+     * 执行verticle类
+     * @param clazz
+     * @param vertx
+     * @return 
+     */
+    public static Future<String> run(Class clazz, Vertx vertx) {
+        return run(clazz, vertx, new JsonObject());
+    }
+
+    /**
+     * 执行verticle类
+     * @param clazz
+     * @param vertx
+     * @param config
+     * @return 
+     */
+    public static Future<String> run(Class clazz, Vertx vertx, JsonObject config) {
+        if (config.isEmpty()) {
+            return runFull(clazz, vertx,null);
+        } else {
+            DeploymentOptions deploymentOptions = new DeploymentOptions();
+            deploymentOptions.setConfig(config);
+            return runFull(clazz, vertx, deploymentOptions);
+        }
+    }
+}
