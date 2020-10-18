3b2a10d1a9bd6122f04c9c92fa03dbd4b71718c7
刘则昌 <914316848@qq.com>
2020-10-18 17:37:12 +0800

    ...

--- vertx-plus-eventbus-rpc/src/main/java/plus/vertx/core/startup/EventBusRpcVerticle.java
+++ vertx-plus-eventbus-rpc/src/main/java/plus/vertx/core/startup/EventBusRpcVerticle.java
@@ -1,7 +1,15 @@
 package plus.vertx.core.startup;
 
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+
 /**
  * Created by crazyliu on 2020/10/17.
  */
 public class EventBusRpcVerticle extends BaseStart {
+    @Override
+    public Future<Void> action(Vertx vertx, Promise<Void> result) {
+        return super.action(vertx, result);
+    }
 }
