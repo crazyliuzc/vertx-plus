8991deb8cf0b447585f0087689a35c2671ddd93f
刘则昌 <914316848@qq.com>
2020-10-19 19:53:24 +0800

    ...

---
+++ vertx-plus-eventbus-rpc/src/main/java/plus/vertx/core/support/eventBusRpc/MessageStyle.java
@@ -0,0 +1,26 @@
+package plus.vertx.core.support.eventBusRpc;
+
+import io.vertx.core.Handler;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 接收消息处理类(公共)
+ *
+ * @author 刘则昌
+ */
+public class MessageStyle implements Handler<Message<JsonObject>> {
+    public static final Logger LOGGER = LoggerFactory.getLogger(MessageStyle.class);
+
+    @Override
+    public void handle(Message<JsonObject> msg) {
+        if (msg.body() != null) {
+//            msg.reply(new JsonObject().put("code",HttpCode.OK.value()));
+//            msg.fail(HttpCode.BAD_REQUEST.value(), "consul set fail");
+        } else {
+//            msg.fail(HttpCode.BAD_REQUEST.value(), "parameter could not be found");
+        }
+    }
+}
