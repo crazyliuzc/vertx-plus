98bdb32ab27f030195456fb11c341f467e6eef86
刘则昌 <914316848@qq.com>
2020-11-09 20:41:52 +0800

    ...

---
+++ vertx-plus-eventbus-rpc/src/main/java/plus/vertx/core/support/eventBusRpc/Priority.java
@@ -0,0 +1,19 @@
+package plus.vertx.core.support.eventBusRpc;
+
+/**
+ * Job priority enum class
+ */
+public enum Priority {
+    LOW(10), NORMAL(0), MEDIUM(-5), HIGH(-10), CRITICAL(-15);
+
+    private int value;
+
+    Priority(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
